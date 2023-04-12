ac9e21142e002f76adc1eb648fead1425fdf4a0b
Anders Sildnes <andsild@posteo.net>
2023-04-12 09:50:39 +0200

    Add worker size to train script

--- run_train.sh
+++ run_train.sh
@@ -2,15 +2,16 @@
 
 set -xe
 
-while getopts e:n:m: flag
+while getopts e:w:n:m: flag
 do
     case "${flag}" in
         e) epoch=${OPTARG};;
         n) n=${OPTARG};;
         m) m=${OPTARG};;
+        w) w=${OPTARG};;
         *) true;;
     esac
 done
 
 # torchrun ./train_ssl.py --data_dir /Data/winter_school/data_dir/ --split_dir my_split_dir/ --batch_slide_num "${n}" --cos --out_dir my_output/ --gpu 0 --epochs "${epoch}" --batch-size "${m}" --moco-t 0.07 --moco-m 0.999
-ipython ./train_ssl.py -- --data_dir /Data/winter_school/data_dir/ --split_dir my_split_dir/ --batch_slide_num "${n}" --cos --out_dir my_output/ --epochs "${epoch}" --batch-size "${m}" --moco-t 0.07 --moco-m 0.999
+ipython ./train_ssl.py -- --data_dir /Data/winter_school/data_dir/ --split_dir my_split_dir/ --batch_slide_num "${n}" --cos --out_dir my_output/ --epochs "${epoch}" --batch-size "${m}" --moco-t 0.07 --moco-m 0.999 --workers "${w:-16}"
