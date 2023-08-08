8c0fff807b990bf04ca7a2516f4a17ce154d22b9
Anders Sildnes <andsild@posteo.net>
2023-08-08 16:13:49 +0200

    a

--- feature_extraction/extract_embeddings.py
+++ feature_extraction/extract_embeddings.py
@@ -1,7 +1,10 @@
+import glob
 import os
 import sys
 
 import numpy as np
+from monai.data import DataLoader, Dataset
+from sklearn.model_selection import train_test_split
 
 sys.path.append('./')
 
@@ -12,30 +15,27 @@ import torch
 import torch.nn as nn
 from collections import defaultdict
 from tqdm import tqdm
-from dataset.dataloader import TCGA_CPTAC_Bag_Dataset
 from network.inception_v4 import InceptionV4
 from pathlib import Path
 
 if torch.cuda.is_available():
     device = 'cuda'
 else:
-    device = 'cpu'
+    raise RuntimeError("cuda not found, use different comp :-(")
 print(device)
-
 
 def ensure_dir_exists(path):
     dest_dir = os.path.dirname(path)
     if not os.path.exists(dest_dir):
         Path(dest_dir).mkdir(parents=True, exist_ok=True)
 
-def get_embeddings_bagging(feature_extractor, subtype_model, data_set):
+def get_embeddings_bagging(feature_extractor, subtype_model, dl, do_outcomes):
     embedding_dict = defaultdict(list)
     outcomes_dict = defaultdict(list)
     feature_extractor.eval()
-    data_loader = torch.utils.data.DataLoader(data_set, batch_size=256, shuffle=False, num_workers=torch.cuda.device_count())
     with torch.no_grad():
         count = 1
-        for batch in tqdm(data_loader, position=0, leave=True, desc="processing batch"):
+        for batch in tqdm(dl, position=0, leave=True, desc="processing batch"):
             count += 1
             # bag idx is same as slide_id. 0 is first slide, 1 is second, etc
             img_batch, _, bag_idx = batch
@@ -61,7 +61,8 @@ def get_embeddings_bagging(feature_extractor, subtype_model, data_set):
                     case_id = '-'.join(slide_id.split('-', 3)[:3])
                 else:
                     case_id = slide_id.rsplit('-', 1)[0]
-                outcomes_dict[slide_index] = annotations[case_id]
+                if do_outcomes:
+                    outcomes_dict[slide_index] = annotations[case_id]
                 embedding_dict[slide_id].append(feat[i][np.newaxis, :])
         # The next for loop is more about making tensors into numpy arrays. We prune away the first dimension which doesn't need to exist
         # it is not merging all tiles from slides
@@ -95,14 +96,16 @@ parser = argparse.ArgumentParser(description='Extract embeddings ')
 parser.add_argument('--feature_extractor_dir', default='./pretrained/checkpoint.pth.tar', type=str, help='path to feature extractor, which will extract features from tiles')
 parser.add_argument('--subtype_model_dir', default='./subtype_cls/checkpoint.pth.tar', type=str, help='path to subtype model, which will differentiate tumor and normal')
 parser.add_argument('--src_dir', type=str, help='path to preprocessed slide images')
+parser.add_argument('--outcomes', default=False, type=bool, action=argparse.BooleanOptionalAction,
+                    metavar='O', help='whether to consider outcomes or not', dest='do_outcomes')
 parser.add_argument('--out_dir', default='./out', type=str, help='path to save extracted embeddings')
 
 args = parser.parse_args()
 
-tcga_annotation = pickle.load(open('./TCGA/recurrence_annotation.pkl', 'rb'))
 # cptac_annotation = pickle.load(open('../CPTAC/recurrence_annotation.pkl', 'rb'))
 # annotations = {**tcga_annotation, **cptac_annotation}
-annotations = {**tcga_annotation}
+if args.do_outcomes:
+    annotations = {**pickle.load(open('./TCGA/recurrence_annotation.pkl', 'rb'))}
 feature_extractor = InceptionV4(num_classes=128)
 load_pretrained(feature_extractor, args.feature_extractor_dir)
 feature_extractor.to('cuda')
@@ -116,20 +119,35 @@ subtype_model.last_linear = nn.Identity()
 subtype_model.load_state_dict(cancer_subtype_model_load)
 subtype_model = nn.DataParallel(subtype_model, device_ids=device_ids)
 
+all_data = []
+for directory in glob.glob(f"{args.src_dir}{os.sep}*"):
+    for file in glob.glob(f"{directory}{os.sep}**{os.sep}*", recursive=True):
+        if os.path.isfile(file):
+            all_data.append({"image": file})
 
-train_dataset = TCGA_CPTAC_Bag_Dataset(args.src_dir, 'train')
-val_dataset = TCGA_CPTAC_Bag_Dataset(args.src_dir, 'val')
-test_dataset = TCGA_CPTAC_Bag_Dataset(args.src_dir, 'test')
+train_data, test_data = train_test_split(all_data, test_size=0.1, random_state=42)
+# note that we split the train data again, not the entire dataset
+train_data, validation_data = train_test_split(train_data, test_size=0.1, random_state=42)
+ds_train = Dataset(train_data)
+ds_val = Dataset(validation_data),
+ds_test = Dataset(test_data)
+
+dl_train = DataLoader(ds_train, batch_size=256, num_workers=torch.cuda.device_count(), shuffle=False)
+dl_val = DataLoader(ds_val, batch_size=256, num_workers=torch.cuda.device_count(), shuffle=False)
+dl_test = DataLoader(ds_test, batch_size=256, num_workers=torch.cuda.device_count(), shuffle=False)
 
 model_name = condssl.builder.MoCo.__name__
 data_dir_name = args.data_dir.split(os.sep)[-1]
-ensure_dir_exists(os.path.join(args.out_dir, model_name, data_dir_name, "embeddings"))
-ensure_dir_exists(os.path.join(args.out_dir, model_name, data_dir_name, "outcomes"))
-
 with torch.no_grad():
-    for name, data_set in list(zip(['train', 'val', 'test'], [train_dataset, val_dataset, test_dataset]))[2:]:
+    for name, data_set in list(zip(['train', 'val', 'test'], [ds_train, ds_val, ds_test]))[2:]:
         print(name)
-        embedding_dict, outcomes_dict = get_embeddings_bagging(feature_extractor, subtype_model, data_set)
-        pickle.dump(embedding_dict, open(os.path.join(args.out_dir, model_name,  "embeddings", f"{name}_{data_dir_name}_embedding.pkl"), 'wb'), protocol=4)
-        pickle.dump((outcomes_dict), open(os.path.join(args.out_dir, model_name, "outcomes", f"{name}_{data_dir_name}_outcomes.pkl"), 'wb'), protocol=4)
-
+        embedding_dict, outcomes_dict = get_embeddings_bagging(feature_extractor, subtype_model, data_set, args.do_outcomes)
+        embedding_dest_path = os.path.join(args.out_dir, model_name,  "embeddings", f"{name}_{data_dir_name}_embedding.pkl")
+        ensure_dir_exists(embedding_dest_path)
+        pickle.dump(embedding_dict, open(embedding_dest_path, 'wb'), protocol=4)
+        if args.do_outcomes:
+            ensure_dir_exists(os.path.join(args.out_dir, model_name, data_dir_name, "outcomes"))
+            outcomes_dest_path = os.path.join(args.out_dir, model_name, "outcomes", f"{name}_{data_dir_name}_outcomes.pkl")
+            pickle.dump((outcomes_dict), open(outcomes_dest_path, 'wb'), protocol=4)
+        else:
+            print("Not dumping outcomes: args.do_outcomes is False")
