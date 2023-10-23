03bb61b036cea77ec2c7a9d5fddd67f693d50987
Anders Sildnes <andsild@posteo.net>
2023-10-23 11:10:26 +0200

    Owe no man any thing... 		-- Romans 13:8

--- process_models.sh
+++ process_models.sh
@@ -15,6 +15,6 @@ do
   out_dir="analysis_/out${m}${n}${o}${c}"
   set -xe
   ipython feature_extraction/extract_embeddings.py --src_dir "${SRC_DIR}" --feature_extractor "${model}" --out_dir "${out_dir}"
-  ipython feature_extraction/get_clusters.py -- --embeddings_path "out$m$n$o/MoCo/tiles/embeddings/test_tiles_embedding.pkl" --out_dir "${out_dir}"
+  ipython feature_extraction/get_clusters.py -- --embeddings_path "${out_dir}/MoCo/tiles/embeddings/test_tiles_embedding.pkl" --out_dir "${out_dir}"
   ipython survival_models/cox.py --  --embeddings_dir "${out_dir}/MoCo/tiles/embeddings/"
 done
