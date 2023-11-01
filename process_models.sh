27c4bb086b428e3c4667e36117078aace23443f1
Anders Sildnes <andsild@posteo.net>
2023-11-01 17:28:55 +0100

    "Here comes Mr. Bill's dog." 		-- Narrator, Saturday Night Live

--- process_models.sh
+++ process_models.sh
@@ -9,7 +9,9 @@ else
   SRC_DIR="${1}"
 fi
 
-mkdir analysis_out/
+SHA="$(git rev-parse HEAD)"
+analysis_dir="analysis_out_$SHA/"
+mkdir "${analysis_dir}"
 
 for model in out/MoCo/tiles/model/*.tar
 do
@@ -18,7 +20,7 @@ do
   o="$(basename "${model}" | command grep -Eo "o[0-9]+")"
   c="$(basename "${model}" | command grep -Eo "True|False")"
   echo "$m $n $c $o"
-  out_dir="analysis_out/out${m}${n}${o}${c}"
+  out_dir="${analysis_dir}/out${m}${n}${o}${c}"
   #set -xe
   ipython feature_extraction/extract_embeddings.py -- --src_dir "${SRC_DIR}" --feature_extractor "${model}" --out_dir "${out_dir}"
   ipython feature_extraction/get_clusters.py -- --embeddings_path "${out_dir}/MoCo/tiles/embeddings/test_tiles_embedding.pkl" --out_dir "${out_dir}"
