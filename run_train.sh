f4a0b77e577d12ea2c31799c60c8f542c31afc77
Anders Sildnes <andsild@posteo.net>
2023-11-27 16:24:33 +0100

    a

--- run_train.sh
+++ run_train.sh
@@ -1,24 +1,38 @@
 #!/usr/bin/env bash
 
-while getopts e:w:n:m:o:s:c: flag
+#conda activate conditional_ssl_hist
+
+if [[ -z "${1}" ]]
+then
+  SRC_DIR="${HOME}/TCGA_LUSC/preprocessed/TCGA/tiles/"
+else
+  SRC_DIR="${1}"
+fi
+
+SHA="$(git rev-parse HEAD)"
+model_out="model_out$SHA/"
+mkdir "${model_out}"
+
+#for model in out/MoCo/tiles/model/*.tar
+#for model in out/MoCo/tiles/model/checkpoint_MoCo_tiles_0200_True_m128_n4.pth.tar out/MoCo/tiles/model/checkpoint_MoCo_tiles_0200_False_m128_n0_o0.pth.tar
+for model in m256n0o4True m256n0o0False # m128n0o4True m128n0o0False 
 do
-    case "${flag}" in
-        e) epoch=${OPTARG};;
-        n) n=${OPTARG};;
-        m) m=${OPTARG};;
-        w) w=${OPTARG};;
-        s) s=${OPTARG};;
-        c) c=${OPTARG};;
-        o) o=${OPTARG};;
-        *) true;;
-    esac
+  m="$(basename "${model}" | command grep -Eo 'm[0-9]+' | cut -c2-)"
+  n="$(basename "${model}" | command grep -Eo "n[0-9]+" | cut -c2-)"
+  o="$(basename "${model}" | command grep -Eo "o[0-9]+" | cut -c2-)"
+  c="$(basename "${model}" | command grep -Eo "True|False")"
+  out_dir="${model_dir}/out${m}${n}${o}${c}"
+  mkdir "${out_dir}"
+  echo "m:$m n:$n o:$o c:$c" | tee "$out_dir/train_params.txt"
+  set -xe
+  python ./train_model/train_ssl.py \
+	--data-dir "${SRC_DIR}" \
+	--batch_slide_num "${n}" \
+	--out-dir "${out_dir}" 
+        --batch-size "${m}" \
+        --condition "${c}" \
+      	--workers 6
+  set +xe
 done
 
 set -xe
-python ./train_model/train_ssl.py \
-	--data-dir "${s:-/data/lung_scc/}" \
-	--batch_slide_num "${n:-4}" --cos \
-	--out-dir "${o:-/output}" --epochs "${epoch:-100}" --batch-size "${m:-64}" \
-       	--moco-t 0.07 --moco-m 0.999 \
-        --condition "${c:-True}" \
-      	--workers "${w:-16}"
