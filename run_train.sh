cfab9179cbb5346e5525850e425656791bdc60bc
Anders Sildnes <andsild@posteo.net>
2023-12-05 16:17:42 +0100

    a

--- run_train.sh
+++ run_train.sh
@@ -13,15 +13,14 @@ SHA="$(git rev-parse HEAD)"
 model_out="model_out$SHA/"
 mkdir "${model_out}"
 
-#for model in out/MoCo/tiles/model/*.tar
-#for model in out/MoCo/tiles/model/checkpoint_MoCo_tiles_0200_True_m128_n4.pth.tar out/MoCo/tiles/model/checkpoint_MoCo_tiles_0200_False_m128_n0_o0.pth.tar
-for model in m256n0o4True m256n0o0False # m128n0o4True m128n0o0False 
+for model in m256n0o4k256True m256n0o0k256False m256n0o0k65536False
 do
   m="$(basename "${model}" | command grep -Eo 'm[0-9]+' | cut -c2-)"
   n="$(basename "${model}" | command grep -Eo "n[0-9]+" | cut -c2-)"
   o="$(basename "${model}" | command grep -Eo "o[0-9]+" | cut -c2-)"
+  k="$(basename "${model}" | command grep -Eo "k[0-9]+" | cut -c2-)"
   c="$(basename "${model}" | command grep -Eo "True|False")"
-  echo "m:$m n:$n o:$o c:$c"
+  echo "m:$m n:$n o:$o k:$k c:$c"
   if [[ "$c" == True ]]
   then
 	  cond="--condition"
@@ -35,10 +34,11 @@ do
 	--batch_inst_num "${o}" \
 	--out-dir "${model_out}" \
 	--batch-size "${m}" \
+	--moco-k "${k}" \
 	--epochs 1 \
   	--moco-k "${m}" \
 	"${cond}" \
 	--workers 6 \
-	    | tee "${model_out}/train_ssl_${m}_${n}_${o}_${c}.log"
+	    | tee "${model_out}/train_ssl_${m}_${n}_${o}_${c}_${k}.log"
   set +xe
 done
