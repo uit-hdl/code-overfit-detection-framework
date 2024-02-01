96e3606e353f7810c5d0474bff8a6b3f2a9f7434
Anders Sildnes <andsild@posteo.net>
2024-02-01 16:17:43 +0100

    It's all in the mind, ya know.

--- train_model/train_regular.py
+++ train_model/train_regular.py
@@ -13,8 +13,10 @@ import time
 import warnings
 
 import monai.transforms as mt
+from monai.metrics import ConfusionMatrixMetric
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 import torch.backends.cudnn as cudnn
 import torch.nn as nn
 import torchvision.transforms as transforms
@@ -28,6 +30,7 @@ from monai.inferers import SimpleInferer
 from monai.networks import eval_mode
 from monai.networks.nets import densenet121
 from monai.transforms import Compose, Activationsd, AsDiscreted
+from sklearn.metrics import roc_curve, confusion_matrix, ConfusionMatrixDisplay
 
 from global_util import build_file_list, ensure_dir_exists, institution_lookup
 
@@ -37,16 +40,11 @@ import contextlib
 no_profiling = contextlib.nullcontext()
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
-# parser.add_argument('data', metavar='DIR',
-#                     help='path to dataset')
 
-parser.add_argument('--epochs', default=100, type=int, metavar='N',
+parser.add_argument('--epochs', default=5, type=int, metavar='N',
                     help='number of total epochs to run')
-parser.add_argument('-b', '--batch-size', default=128, type=int,
-                    metavar='N',
-                    help='mini-batch size (default: 128), this is the total '
-                         'batch size of all GPUs on the current node when '
-                         'using Data Parallel or Distributed Data Parallel')
+parser.add_argument('-b', '--batch-size', default=64, type=int,
+                    metavar='N', help='batch size (default: 128), this is the total batch size of all GPUs on the current node when using Data Parallel or Distributed Data Parallel')
 parser.add_argument('--lr', '--learning-rate', default=0.03, type=float,
                     metavar='LR', help='initial learning rate', dest='lr')
 parser.add_argument('--schedule', default=[120, 160], nargs='*', type=int,
@@ -62,7 +60,8 @@ parser.add_argument('--data-dir', default='./data/', type=str,
 parser.add_argument('--out-dir', default='./out/models/', type=str,
                     help='path to output directory')
 parser.add_argument('--file-list-path', default='./out/files.csv', type=str, help='path to list of file splits')
-parser.add_argument('--slide_annotation_file', default='annotations/slide_label/gdc_sample_sheet.2023-08-14.tsv', type=str, help='"Sample sheet" from TCGA, see README.md for instructions on how to get sheet')
+parser.add_argument('--slide_annotation_file', default='annotations/slide_label/gdc_sample_sheet.2023-08-14.tsv', type=str,
+                    help='"Sample sheet" from TCGA, see README.md for instructions on how to get sheet')
 parser.add_argument('-j', '--workers', default=6, type=int, metavar='N',
                     help='number of data loading workers (default: 6)')
 
@@ -87,7 +86,7 @@ def train(dl_train, dl_val, model, optimizer, max_epochs, out_path, device):
 
     trainer = SupervisedTrainer(
         device=device,
-        max_epochs=5,
+        max_epochs=max_epochs,
         train_data_loader=dl_train,
         network=model,
         optimizer=optimizer,
@@ -140,7 +139,7 @@ def train(dl_train, dl_val, model, optimizer, max_epochs, out_path, device):
 def wrap_data(train_data, val_data, test_data, slide_annotations, labels, batch_size, workers, is_profiling):
     logging.info('Creating dataset')
     grayer = transforms.Grayscale(num_output_channels=3)
-    cropper = transforms.RandomResizedCrop(299, scale=(0.2, 1.))
+    cropper = transforms.RandomResizedCrop(299, scale=(0.2, 1.), antialias=True)
     jitterer = transforms.ColorJitter(brightness=4, contrast=0.4, saturation=0.4, hue=0.01)
 
 
@@ -149,28 +148,28 @@ def wrap_data(train_data, val_data, test_data, slide_annotations, labels, batch_
         return Range(x, methods="__call__")(y) if is_profiling else y
     transformations = mt.Compose(
         [
-            range_func("LoadImage", mt.LoadImaged(["q", "k"], image_only=True)),
-            range_func("EnsureChannelFirst", mt.EnsureChannelFirstd(["q", "k"])),
-            range_func("Crop", mt.Lambdad(["q", "k"], cropper)),
-            range_func("ColorJitter", mt.RandLambdad(["q", "k"], jitterer, prob=0.8)),
-            range_func("Grayscale", mt.RandLambdad(["q", "k"], grayer, prob=0.2)),
-            range_func("Flip0", mt.RandFlipd(["q", "k"], prob=0.5, spatial_axis=0)),
-            range_func("Flip1", mt.RandFlipd(["q", "k"], prob=0.5, spatial_axis=1)),
-            range_func("ToTensor", mt.ToTensord(["q", "k"], track_meta=False)),
-            range_func("EnsureType", mt.EnsureTyped(["q", "k"], track_meta=False)),
+            range_func("LoadImage", mt.LoadImaged(["image"], image_only=True)),
+            range_func("EnsureChannelFirst", mt.EnsureChannelFirstd(["image"])),
+            range_func("Crop", mt.Lambdad(["image"], cropper)),
+            range_func("ColorJitter", mt.RandLambdad(["image"], jitterer, prob=0.8)),
+            range_func("Grayscale", mt.RandLambdad(["image"], grayer, prob=0.2)),
+            range_func("Flip0", mt.RandFlipd(["image"], prob=0.5, spatial_axis=0)),
+            range_func("Flip1", mt.RandFlipd(["image"], prob=0.5, spatial_axis=1)),
+            range_func("ToTensor", mt.ToTensord(["image"], track_meta=False)),
+            range_func("EnsureType", mt.EnsureTyped(["image"], track_meta=False)),
         ]
     )
 
     val_transformations = mt.Compose(
         [
-            mt.LoadImaged(["q", "k"], image_only=True),
-            mt.EnsureChannelFirstd(["q", "k"]),
-            mt.Lambdad(["q", "k"], cropper),
-            mt.RandLambdad(["q", "k"], jitterer, prob=0.8),
-            mt.RandLambdad(["q", "k"], grayer, prob=0.2),
-            mt.RandFlipd(["q", "k"], prob=0.5, spatial_axis=0),
-            mt.RandFlipd(["q", "k"], prob=0.5, spatial_axis=1),
-            mt.ToTensord(["q", "k"], track_meta=False),
+            mt.LoadImaged(["image"], image_only=True),
+            mt.EnsureChannelFirstd(["image"]),
+            mt.Lambdad(["image"], cropper),
+            mt.RandLambdad(["image"], jitterer, prob=0.8),
+            mt.RandLambdad(["image"], grayer, prob=0.2),
+            mt.RandFlipd(["image"], prob=0.5, spatial_axis=0),
+            mt.RandFlipd(["image"], prob=0.5, spatial_axis=1),
+            mt.ToTensord(["image"], track_meta=False),
         ]
     )
 
@@ -198,6 +197,8 @@ def assign_labels(ds, labels, annotations):
         filename = entry['filename']
         slide_id = os.path.basename(os.path.dirname(filename))
         entry['label'] = labels[annotations.loc[slide_id, "Sample Type"]]
+        del entry['q']
+        del entry['k']
     logging.info("Annotations complete")
 
 def main():
@@ -224,6 +225,8 @@ def main():
     labels = slide_annotations["Sample Type"].unique().tolist()
 
     train_data, val_data, test_data = build_file_list(args.data_dir, args.file_list_path)
+    train_data = train_data[:args.batch_size]
+    test_data = test_data[:args.batch_size]
     dl_train, dl_val, dl_test = wrap_data(train_data, val_data, test_data, slide_annotations, labels, args.batch_size, args.workers, args.is_profiling)
 
     if args.seed is not None:
@@ -236,47 +239,69 @@ def main():
                       'You may see unexpected behavior when restarting '
                       'from checkpoints.')
 
-    logging.info("=> creating model '{}'".format('x64'))
-    model = densenet121(spatial_dims=2, in_channels=3, out_channels=2, pretrained=True).to(device)
-    model = model.cuda()
-    optimizer = torch.optim.Adam(model.parameters(), args.lr)
-    logging.info('Model builder done, placed on cuda()')
     logging.info("Batch size: {}".format(args.batch_size))
 
     model_name = "densenet121"
     data_dir_name = list(filter(None, args.data_dir.split(os.sep)))[-1]
     out_path = os.path.join(args.out_dir, model_name, data_dir_name, 'model')
 
-    epoch_loss_values, metric_values = train(dl_train, dl_val, model, optimizer, args.epochs, out_path, device)
+    model = None
+    model_path = os.path.join(out_path, f"network_epoch={args.epochs}.pt")
+    model = densenet121(spatial_dims=2, in_channels=3, out_channels=2, pretrained=True).to(device)
+    if os.path.exists(model_path):
+        logging.info(f"=> loading model '{model_path}'")
+        model.load_state_dict(torch.load(model_path, map_location=device))
+        logging.info('Model builder done, placed on cuda()')
+    else:
+        logging.info("=> creating model '{}'".format('x64'))
+        optimizer = torch.optim.Adam(model.parameters(), args.lr)
+        logging.info('Model builder done, placed on cuda()')
+        epoch_loss_values, metric_values = train(dl_train, dl_val, model, optimizer, args.epochs, out_path, device)
 
-    max_items_to_print = 20 if len(dl_test.dataset) > 20 else len(dl_test.dataset)
+        how_many_batches = len(train_data) // args.batch_size
+        plt.figure(1, (12, 6))
+        plt.subplot(1, 2, 1)
+        plt.title("Iteration Average Loss")
+        x = [how_many_batches * (i + 1) for i in range(len(epoch_loss_values))]
+        y = epoch_loss_values
+        plt.xlabel("Iteration")
+        plt.plot(x, y)
+        plt.grid()
+        plt.subplot(1, 2, 2)
+        plt.title("Val Mean Dice")
+        x = [how_many_batches * (i + 1) for i in range(len(metric_values))]
+        y = metric_values
+        plt.xlabel("Iteration")
+        plt.plot(x, y)
+        plt.grid()
+        #plt.show()
+        plt.savefig(os.path.join(args.out_dir, 'train_stats.png'))
+
+    predictions = []
+    gts = []
+    logging.info('Evaluating model')
     with eval_mode(model):
-        for item in dl_test:
-            prob = torch.squeeze(model(item["image"].to(device)).detach().to("cpu")).numpy()
-            pred = labels[prob.argmax()]
-            gt = labels[(item["label"].detach().item())]
-            print(f"Class prediction is {pred}:{institution_lookup[pred]}. Ground-truth: {gt}:{institution_lookup[gt]}")
-            max_items_to_print -= 1
+        for item in tqdm(dl_test):
+            prob = model(item["image"].to(device)).detach().to("cpu")
+            pred = torch.argmax(prob, dim=1).numpy()
+            predictions += list(x for x in pred)
 
-    how_many_batches = len(train_data) // args.batch_size
+            gt = item["label"].detach().cpu().numpy()
+            gts += list(x for x in gt)
 
-    plt.figure(1, (12, 6))
-    plt.subplot(1, 2, 1)
-    plt.title("Iteration Average Loss")
-    x = [how_many_batches * (i + 1) for i in range(len(epoch_loss_values))]
-    y = epoch_loss_values
-    plt.xlabel("Iteration")
-    plt.plot(x, y)
-    plt.grid()
-    plt.subplot(1, 2, 2)
-    plt.title("Val Mean Dice")
-    x = [how_many_batches * (i + 1) for i in range(len(metric_values))]
-    y = metric_values
-    plt.xlabel("Iteration")
-    plt.plot(x, y)
-    plt.grid()
-    #plt.show()
-    plt.savefig(os.path.join(args.out_dir, 'train_stats.png'))
+    roc = roc_curve(gts, predictions)
+    plt.figure()
+    plt.title("ROC Curve")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.plot(roc[0], roc[1])
+
+    labels = slide_annotations["Sample Type"].unique().tolist()
+    cm = confusion_matrix(gts, predictions, normalize="true", labels=list(range(len(labels))))
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+    disp.plot(ax=plt.subplots(1, 1, facecolor="white")[1])
+
+    plt.show()
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
