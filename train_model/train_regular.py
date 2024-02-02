a9bcf445d564b1727d6caaf7dc4a2ea1e9e63521
Anders Sildnes <andsild@posteo.net>
2024-02-02 12:56:31 +0100

    FORTUNE PROVIDES QUESTIONS FOR THE GREAT ANSWERS: #19 A:	To be or not to be. Q:	What is the square root of 4b^2?

--- train_model/train_regular.py
+++ train_model/train_regular.py
@@ -2,7 +2,9 @@
 # coding: utf-8
 
 import logging
+from collections.abc import Iterable
 import argparse
+import matplotlib.ticker as mticker
 import csv
 import os.path
 import pickle
@@ -13,6 +15,7 @@ import time
 import warnings
 
 import monai.transforms as mt
+import torch
 from monai.metrics import ConfusionMatrixMetric
 import numpy as np
 import pandas as pd
@@ -22,26 +25,27 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 from ignite.metrics import Accuracy
 from matplotlib import pyplot as plt
-from monai.data import DataLoader, Dataset, set_track_meta
+from monai.data import DataLoader, Dataset, set_track_meta, create_test_image_2d
 from monai.engines import SupervisedTrainer, SupervisedEvaluator
 from monai.engines.workflow import Events
-from monai.handlers import StatsHandler, from_engine, ValidationHandler, CheckpointSaver
+from monai.handlers import StatsHandler, from_engine, ValidationHandler, CheckpointSaver, MeanDice, \
+    TensorBoardImageHandler, TensorBoardStatsHandler
 from monai.inferers import SimpleInferer
 from monai.networks import eval_mode
 from monai.networks.nets import densenet121
-from monai.transforms import Compose, Activationsd, AsDiscreted
+from monai.transforms import Compose, Activationsd, AsDiscreted, EnsureTyped, EnsureChannelFirstd
 from sklearn.metrics import roc_curve, confusion_matrix, ConfusionMatrixDisplay
 
 from global_util import build_file_list, ensure_dir_exists, institution_lookup
 
 from train_util import *
-from monai.utils import Range
+from monai.utils import Range, CommonKeys
 import contextlib
 no_profiling = contextlib.nullcontext()
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 
-parser.add_argument('--epochs', default=5, type=int, metavar='N',
+parser.add_argument('--epochs', default=3, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('-b', '--batch-size', default=64, type=int,
                     metavar='N', help='batch size (default: 128), this is the total batch size of all GPUs on the current node when using Data Parallel or Distributed Data Parallel')
@@ -62,8 +66,8 @@ parser.add_argument('--out-dir', default='./out/models/', type=str,
 parser.add_argument('--file-list-path', default='./out/files.csv', type=str, help='path to list of file splits')
 parser.add_argument('--slide_annotation_file', default='annotations/slide_label/gdc_sample_sheet.2023-08-14.tsv', type=str,
                     help='"Sample sheet" from TCGA, see README.md for instructions on how to get sheet')
-parser.add_argument('-j', '--workers', default=6, type=int, metavar='N',
-                    help='number of data loading workers (default: 6)')
+parser.add_argument('-j', '--workers', default=0, type=int, metavar='N',
+                    help='number of data loading workers (default: 0)')
 
 def save_data_to_csv(data, filename, label):
     ensure_dir_exists(filename)
@@ -73,14 +77,36 @@ def save_data_to_csv(data, filename, label):
         for e, l in enumerate(data):
             csvwriter.writerow([e,l])
 
+def from_engine_custom(keys, device):
+    """
+        Encapsulate data in dimensions to satisfy MONAI API
+    """
+    def _wrapper(data):
+        ret = [[i[k] for i in data] for k in keys]
+        return list(map(lambda l: l.unsqueeze(0), ret[0])), list(map(lambda l: torch.tensor(l).unsqueeze(0).to(device), ret[1]))
+
+    return _wrapper
 
 
 def train(dl_train, dl_val, model, optimizer, max_epochs, out_path, device):
-    val_postprocessing = Compose([Activationsd(keys="pred", sigmoid=True), AsDiscreted(keys="pred", threshold=0.5)])
+    val_postprocessing = Compose([EnsureTyped(keys=CommonKeys.PRED),
+                                  AsDiscreted(keys=CommonKeys.PRED, argmax=True),
+                                  ])
+
     evaluator = SupervisedEvaluator(
         device=device,
         val_data_loader=dl_val,
         network=model,
+        # No stats for iteration
+        val_handlers=[
+            StatsHandler(tag_name="train_log", output_transform=lambda x: None),
+            TensorBoardStatsHandler(log_dir=os.path.join(out_path, "runs"), output_transform=lambda x: x),
+            CheckpointSaver(save_dir=os.path.join(out_path, "runs"), save_dict={"net": model}, save_key_metric=True),
+            ],
+        additional_metrics={"val_acc": Accuracy(output_transform=from_engine([CommonKeys.PRED, CommonKeys.LABEL]))},
+        key_val_metric={
+            "val_mean_dice": MeanDice(output_transform=from_engine_custom(keys=[CommonKeys.PRED, CommonKeys.LABEL], device=device))
+        },
         postprocessing=val_postprocessing,
     )
 
@@ -92,9 +118,9 @@ def train(dl_train, dl_val, model, optimizer, max_epochs, out_path, device):
         optimizer=optimizer,
         loss_function=torch.nn.CrossEntropyLoss(),
         inferer=SimpleInferer(),
-        key_train_metric={"train_acc": Accuracy(output_transform=from_engine(["pred", "label"]))},
-        train_handlers=[StatsHandler(tag_name="train_loss", output_transform=from_engine(["loss"], first=True)),
-                        ValidationHandler(1, evaluator, epoch_level = True),
+        key_train_metric={"train_acc": Accuracy(output_transform=from_engine([CommonKeys.PRED, CommonKeys.LABEL]))},
+        train_handlers=[StatsHandler(tag_name="train_loss", output_transform=from_engine([CommonKeys.LOSS], first=True)),
+                        ValidationHandler(1, evaluator),
                         CheckpointSaver(save_dir=out_path, save_dict={'network': model}, save_interval=1)
                         ],
     )
@@ -103,10 +129,16 @@ def train(dl_train, dl_val, model, optimizer, max_epochs, out_path, device):
     batchSizes = []
     epoch_loss_values = []
     metric_values = []
+    mean_dice_values = []
+    mean_val_acc = []
+
+    @evaluator.on(Events.EPOCH_COMPLETED)
+    def log_validation(engine):
+        mean_dice_values.append(engine.state.metrics["val_mean_dice"])
+        mean_val_acc.append(engine.state.metrics["val_acc"])
 
     @trainer.on(Events.ITERATION_COMPLETED)
     def log_training(engine):
-        batch_loss = engine.state.output
         loss = np.average([o["loss"] for o in engine.state.output])
         batch_len = len(engine.state.batch[0])
 
@@ -129,11 +161,11 @@ def train(dl_train, dl_val, model, optimizer, max_epochs, out_path, device):
         engine.state.iteration = 0
 
         # fetch and report the validation metrics
-        print(f"evaluation for epoch {engine.state.epoch}: averageLoss: {overallAverageLoss}, epoch_loss_values: {epoch_loss_values}, training accuracy: {train_acc}")
+        logging.info(f"evaluation for epoch {engine.state.epoch}: averageLoss: {overallAverageLoss}, epoch_loss_values: {epoch_loss_values}, training accuracy: {train_acc}")
 
     trainer.run()
 
-    return epoch_loss_values, metric_values
+    return epoch_loss_values, metric_values, mean_dice_values, mean_val_acc
 
 
 def wrap_data(train_data, val_data, test_data, slide_annotations, labels, batch_size, workers, is_profiling):
@@ -148,28 +180,29 @@ def wrap_data(train_data, val_data, test_data, slide_annotations, labels, batch_
         return Range(x, methods="__call__")(y) if is_profiling else y
     transformations = mt.Compose(
         [
-            range_func("LoadImage", mt.LoadImaged(["image"], image_only=True)),
-            range_func("EnsureChannelFirst", mt.EnsureChannelFirstd(["image"])),
-            range_func("Crop", mt.Lambdad(["image"], cropper)),
-            range_func("ColorJitter", mt.RandLambdad(["image"], jitterer, prob=0.8)),
-            range_func("Grayscale", mt.RandLambdad(["image"], grayer, prob=0.2)),
-            range_func("Flip0", mt.RandFlipd(["image"], prob=0.5, spatial_axis=0)),
-            range_func("Flip1", mt.RandFlipd(["image"], prob=0.5, spatial_axis=1)),
-            range_func("ToTensor", mt.ToTensord(["image"], track_meta=False)),
-            range_func("EnsureType", mt.EnsureTyped(["image"], track_meta=False)),
+            range_func("LoadImage", mt.LoadImaged([CommonKeys.IMAGE], image_only=True)),
+            range_func("EnsureChannelFirst", mt.EnsureChannelFirstd([CommonKeys.IMAGE])),
+            range_func("Crop", mt.Lambdad([CommonKeys.IMAGE], cropper)),
+            range_func("ColorJitter", mt.RandLambdad([CommonKeys.IMAGE], jitterer, prob=0.8)),
+            range_func("Grayscale", mt.RandLambdad([CommonKeys.IMAGE], grayer, prob=0.2)),
+            range_func("Flip0", mt.RandFlipd([CommonKeys.IMAGE], prob=0.5, spatial_axis=0)),
+            range_func("Flip1", mt.RandFlipd([CommonKeys.IMAGE], prob=0.5, spatial_axis=1)),
+            range_func("ToTensor", mt.ToTensord([CommonKeys.IMAGE], track_meta=False)),
+            range_func("EnsureType", mt.EnsureTyped([CommonKeys.IMAGE, CommonKeys.LABEL], track_meta=False)),
         ]
     )
 
     val_transformations = mt.Compose(
         [
-            mt.LoadImaged(["image"], image_only=True),
-            mt.EnsureChannelFirstd(["image"]),
-            mt.Lambdad(["image"], cropper),
-            mt.RandLambdad(["image"], jitterer, prob=0.8),
-            mt.RandLambdad(["image"], grayer, prob=0.2),
-            mt.RandFlipd(["image"], prob=0.5, spatial_axis=0),
-            mt.RandFlipd(["image"], prob=0.5, spatial_axis=1),
-            mt.ToTensord(["image"], track_meta=False),
+            mt.LoadImaged([CommonKeys.IMAGE]),
+            mt.EnsureChannelFirstd([CommonKeys.IMAGE]),
+            # mt.Lambdad([CommonKeys.IMAGE], cropper),
+            # mt.RandLambdad([CommonKeys.IMAGE], jitterer, prob=0.8),
+            # mt.RandLambdad([CommonKeys.IMAGE], grayer, prob=0.2),
+            # mt.RandFlipd([CommonKeys.IMAGE], prob=0.5, spatial_axis=0),
+            # mt.RandFlipd([CommonKeys.IMAGE], prob=0.5, spatial_axis=1),
+            mt.ToTensord([CommonKeys.IMAGE], track_meta=False),
+            mt.EnsureTyped([CommonKeys.IMAGE, CommonKeys.LABEL], track_meta=False),
         ]
     )
 
@@ -194,11 +227,12 @@ def assign_labels(ds, labels, annotations):
     logging.info("Processing annotations")
     labels = {l: i for i, l in enumerate(labels)}
     for entry in ds:
-        filename = entry['filename']
+        filename = entry[CommonKeys.IMAGE]
         slide_id = os.path.basename(os.path.dirname(filename))
-        entry['label'] = labels[annotations.loc[slide_id, "Sample Type"]]
+        entry[CommonKeys.LABEL] = labels[annotations.loc[slide_id, "Sample Type"]]
         del entry['q']
         del entry['k']
+        del entry['filename']
     logging.info("Annotations complete")
 
 def main():
@@ -225,8 +259,9 @@ def main():
     labels = slide_annotations["Sample Type"].unique().tolist()
 
     train_data, val_data, test_data = build_file_list(args.data_dir, args.file_list_path)
-    #train_data = train_data[:args.batch_size]
-    #test_data = test_data[:args.batch_size]
+    train_data = train_data[:args.batch_size * 2]
+    val_data = val_data[:args.batch_size * 2]
+    test_data = test_data[:args.batch_size]
     dl_train, dl_val, dl_test = wrap_data(train_data, val_data, test_data, slide_annotations, labels, args.batch_size, args.workers, args.is_profiling)
 
     if args.seed is not None:
@@ -248,7 +283,7 @@ def main():
     model = None
     model_path = os.path.join(out_path, f"network_epoch={args.epochs}.pt")
     model = densenet121(spatial_dims=2, in_channels=3, out_channels=2, pretrained=True).to(device)
-    if os.path.exists(model_path):
+    if os.path.exists(model_path) and False:
         logging.info(f"=> loading model '{model_path}'")
         model.load_state_dict(torch.load(model_path, map_location=device))
         logging.info('Model builder done, placed on cuda()')
@@ -256,26 +291,46 @@ def main():
         logging.info("=> creating model '{}'".format('x64'))
         optimizer = torch.optim.Adam(model.parameters(), args.lr)
         logging.info('Model builder done, placed on cuda()')
-        epoch_loss_values, metric_values = train(dl_train, dl_val, model, optimizer, args.epochs, out_path, device)
+        epoch_loss_values, metric_values, mean_dice_values, mean_val_acc = train(dl_train, dl_val, model, optimizer, args.epochs, out_path, device)
 
         how_many_batches = len(train_data) // args.batch_size
-        plt.figure(1, (12, 6))
-        plt.subplot(1, 2, 1)
-        plt.title("Iteration Average Loss")
-        x = [how_many_batches * (i + 1) for i in range(len(epoch_loss_values))]
+        fig = plt.figure(1, (24, 6))
+        fig.subplots_adjust(hspace=0.4, wspace=0.4)
+        ax = fig.add_subplot(2, 2, 1)
+        ax.set_title("Iteration Average Loss")
         y = epoch_loss_values
-        plt.xlabel("Iteration")
-        plt.plot(x, y)
-        plt.grid()
-        plt.subplot(1, 2, 2)
-        plt.title("Val Mean Dice")
-        x = [how_many_batches * (i + 1) for i in range(len(metric_values))]
+        # set label of x-axis
+        ax.axes.set_xlabel("Iteration")
+        ax.xaxis.set_major_locator(mticker.MaxNLocator(integer=True))
+        ax.plot(range(len(y)), y)
+        ax.xaxis.set_major_locator(mticker.MaxNLocator(integer=True))
+        ax = fig.add_subplot(2, 2, 2)
+        ax.set_title("Val Mean Dice")
         y = metric_values
-        plt.xlabel("Iteration")
-        plt.plot(x, y)
-        plt.grid()
-        #plt.show()
-        plt.savefig(os.path.join(args.out_dir, 'train_stats.png'))
+        ax.axes.set_xlabel("Iteration")
+        ax.plot(y)
+        ax = fig.add_subplot(2, 2, 3)
+        # plot the mean dice values
+        ax.set_title("Val Mean Dice")
+        ax.xaxis.set_major_locator(mticker.MultipleLocator(1))
+        x = len(mean_dice_values)
+        y = mean_dice_values
+        ax.axes.set_xlabel("Epoch")
+        ax.axes.set_ylim(0, 1)
+        ax.plot(y)
+        ax = fig.add_subplot(2, 2, 4)
+        # plot the mean validation accuracy
+        ax.set_title("Val Accuracy")
+        x = len(mean_val_acc)
+        y = mean_val_acc
+        ax.axes.set_ylim(0, 1)
+        ax.axes.set_xlabel("Epoch")
+        ax.plot(y)
+        ax.xaxis.set_major_locator(mticker.MaxNLocator(integer=True))
+        fig.gca().xaxis.set_major_locator(mticker.MaxNLocator(integer=True))
+        fig.show()
+        sys.exit(0)
+        #plt.savefig(os.path.join(args.out_dir, 'train_stats.png'))
 
     predictions = []
     gts = []
