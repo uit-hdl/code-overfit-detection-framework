4bd38f6f357adad620d3ccb9d0653aee846e60e5
Anders Sildnes <andsild@posteo.net>
2024-05-21 15:59:57 +0200

    a

--- scripts/process_models.sh
+++ scripts/process_models.sh
@@ -41,7 +41,7 @@ test -d "${analysis_dir}" || mkdir -p "${analysis_dir}"
 #for model in "${model_out_dir}"/MoCo/tiles/model/*0200*.tar
 #for model in "${model_out_dir}"/MoCo/tiles/model/*0180*False*.tar
 #for model in "${model_out_dir}"/MoCo/*/model/*0040*False*.tar
-for model in model_out2b1413ba2b3df0bcd9e2c56bdbea8d2c7f875d1e/MoCo/tiles//model/checkpoint_MoCo_tiles_0100_True_m256_n0_o4_K256.pth.tar out-no-color-normalization/MoCo/tiles-no-color-normalization/model/checkpoint_MoCo_tiles-no-color-normalization_0040_False_m64_n0_o0_K64.pth.tar
+for model in $(find out* -name \*checkpoint\*.pth.tar -type f)
 do
   m="$(basename "${model}" | command grep -Eo 'm[0-9]+')"
   n="$(basename "${model}" | command grep -Eo "n[0-9]+")"
@@ -49,8 +49,9 @@ do
   K="$(basename "${model}" | command grep -Eo "K[0-9]+")"
   c="$(basename "${model}" | command grep -Eo "True|False")"
   e="$(basename "${model}" | command grep -Eo "0[0-9]+")"
-  out_dir="${analysis_dir}/out_e${e}${m}${n}${K}${o}${c}"
-  echo "e$e $m $n $c $K $o $out_dir $model"
+  d="$(basename "${model}" | command grep -Eo "tiles[0-9A-Za-z-]?")"
+  out_dir="${analysis_dir}/out_${d}e${e}${m}${n}${K}${o}${c}"
+  echo "e$e $d $m $n $c $K $o $out_dir $model"
   set -xe
   # TODO: produce no-color-normalizatio CPTAC
   test -e "${out_dir}"/inceptionv4/$(basename ${model})/cptac_*_embedding.pkl || \
@@ -61,11 +62,11 @@ do
   test -d "${out_dir}"/web_${bsrc}/ || \
 	  ipython feature_extraction/get_clusters.py -- --embeddings-path-val "${out_dir}"/inceptionv4/$(basename ${model})/val_${bsrc}_embedding.pkl --embeddings-path-train "${out_dir}"/inceptionv4/$(basename ${model})/train_${bsrc}_embedding.pkl --embeddings-path-test "${out_dir}"/inceptionv4/$(basename ${model})/test_${bsrc}_embedding.pkl --out-dir "${out_dir}" --number-of-images 2999 --histogram-bins 20
 
-  test -d "${out_dir}"/MoCo/*/model/relabelled_my_inst*${bsrc}*/ || \
-	  ipython train_model/relabelling.py -- --epochs 5 --src-dir "${SRC_DIR}" --out-dir "${out_dir}" --feature_extractor "${model}" --label-key "my_inst"
-  relabelled_model="$(find "${out_dir}"/MoCo/*/model/relabelled_my_inst*${bsrc}*/ -name \*network_epoch\=5.pt -type f)"
-  test -e "${out_dir}"/relabelled_my_inst_*${bsrc}*Institution_*fairness.csv || \
-	  ipython ./misc/fairness_relabelled_my_inst.py -- --feature_extractor "${relabelled_model}" --src_dir "${SRC_DIR}" --out_dir "${out_dir}"
+  #test -d "${out_dir}"/MoCo/${bsrc}/model/relabelled_my_inst*${bsrc}*/ || \
+	  #ipython train_model/relabelling.py -- --epochs 5 --src-dir "${SRC_DIR}" --out-dir "${out_dir}" --feature_extractor "${model}" --label-key "my_inst"
+  #relabelled_model="$(find "${out_dir}"/MoCo/*/model/relabelled_my_inst*${bsrc}*/ -name \*network_epoch\=5.pt -type f)"
+  #test -e "${out_dir}"/relabelled_my_inst_*${bsrc}*Institution_*fairness.csv || \
+	  #ipython ./misc/fairness_relabelled_my_inst.py -- --feature_extractor "${relabelled_model}" --src_dir "${SRC_DIR}" --out_dir "${out_dir}"
 
   test -d "${out_dir}"/MoCo/*/model/relabelled_Sample*${bsrc}*/ || \
 	  ipython train_model/relabelling.py -- --epochs 10 --src-dir "${SRC_DIR}" --out-dir "${out_dir}" --feature_extractor "${model}" --label-key "Sample Type"
@@ -80,22 +81,22 @@ do
   if [[ ! "${SRC_ALT_DIR}" == "${SRC_DIR}" ]]
   then
 	  test -e "${out_dir}"/inceptionv4/$(basename ${model})/test_${balt}_embedding.pkl || \
-		  ipython feature_extraction/extract_embeddings.py -- --src-dir "${SRC_DIR}" --feature-extractor "${model}" --out-dir "${out_dir}"
+		  ipython feature_extraction/extract_embeddings.py -- --src-dir "${SRC_ALT_DIR}" --feature-extractor "${model}" --out-dir "${out_dir}"
 
 	  test -d "${out_dir}"/web_${balt}/ || \
 		  ipython feature_extraction/get_clusters.py -- --embeddings-path-val "${out_dir}"/inceptionv4/$(basename ${model})/val_${balt}_embedding.pkl --embeddings-path-train "${out_dir}"/inceptionv4/$(basename ${model})/train_${balt}_embedding.pkl --embeddings-path-test "${out_dir}"/inceptionv4/$(basename ${model})/test_${balt}_embedding.pkl --out-dir "${out_dir}" --number-of-images 2999 --histogram-bins 20
 
-	  test -d "${out_dir}"/MoCo/*/model/relabelled_my_inst*${balt}*/ || \
-		  ipython train_model/relabelling.py -- --epochs 5 --src-dir "${SRC_DIR}" --out-dir "${out_dir}" --feature_extractor "${model}" --label-key "my_inst"
+	  #test -d "${out_dir}"/MoCo/${balt}/model/relabelled_my_inst*${balt}*/ || \
+		  #ipython train_model/relabelling.py -- --epochs 5 --src-dir "${SRC_ALT_DIR}" --out-dir "${out_dir}" --feature_extractor "${model}" --label-key "my_inst"
 	  relabelled_model="$(find "${out_dir}"/MoCo/*/model/relabelled_my_inst*${balt}*/ -name \*network_epoch\=5.pt -type f)"
-	  test -e "${out_dir}"/relabelled_my_inst_*${balt}*Institution_*fairness.csv || \
-		  ipython ./misc/fairness_relabelled_my_inst.py -- --feature_extractor "${relabelled_model}" --src_dir "${SRC_DIR}" --out_dir "${out_dir}"
+	  #test -e "${out_dir}"/relabelled_my_inst_*${balt}*Institution_*fairness.csv || \
+		  #ipython ./misc/fairness_relabelled_my_inst.py -- --feature_extractor "${relabelled_model}" --src_dir "${SRC_ALT_DIR}" --out_dir "${out_dir}"
 
 	  test -d "${out_dir}"/MoCo/*/model/relabelled_Sample*${balt}*/ || \
-		  ipython train_model/relabelling.py -- --epochs 10 --src-dir "${SRC_DIR}" --out-dir "${out_dir}" --feature_extractor "${model}" --label-key "Sample Type"
+		  ipython train_model/relabelling.py -- --epochs 10 --src-dir "${SRC_ALT_DIR}" --out-dir "${out_dir}" --feature_extractor "${model}" --label-key "Sample Type"
 	  relabelled_model="$(find "${out_dir}"/MoCo/*/model/relabelled_Sample*${balt}*/ -name \*network_epoch\=10.pt -type f)"
 	  test -e "${out_dir}"/relabelled_Sample*${balt}*Institution_fairness.csv || \
-		  ipython ./misc/fairness.py -- --feature_extractor "${relabelled_model}" --src_dir "${SRC_DIR}" --out_dir "${out_dir}"
+		  ipython ./misc/fairness.py -- --feature_extractor "${relabelled_model}" --src_dir "${SRC_ALT_DIR}" --out_dir "${out_dir}"
   fi
 
 
