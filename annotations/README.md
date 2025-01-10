06731449d4bea9b766e0238b5dab9dc0666ecc04
Anders Sildnes <andsild@posteo.net>
2025-01-10 09:52:13 -0600

    Clean up unused files

--- annotations/README.md
+++
@@ -1,9 +0,0 @@
-# Clinical annotations
-## TCGA LUSC
-Download "clinical" from https://portal.gdc.cancer.gov/projects/TCGA-LUSC as a tsv file
-If you add all those slides to the cart, you can then download a "sample sheet" in the "cart section" of TCGA.
-
-... but a more up-to date clinical sheet is in https://pubmed.ncbi.nlm.nih.gov/29625055/
-
-## CPTAC
-Annotations: download "clinical", then "clinical.tsv": [https://portal.gdc.cancer.gov/projects/CPTAC-3](https://portal.gdc.cancer.gov/projects/CPTAC-3) 
--- train_model/train_ssl.py
+++ train_model/train_ssl.py
@@ -95,7 +95,33 @@ import samplers
 from misc.global_util import build_file_list, ensure_dir_exists
 
 from network.inception_v4 import InceptionV4
-from train_util import *
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t() # transpose
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size).item())
+        return res
+
+
+def adjust_learning_rate(optimizer, epoch, lr, cos, schedule, max_epochs):
+    """Decay the learning rate based on schedule"""
+    if cos:  # cosine lr schedule
+        lr *= 0.5 * (1. + math.cos(math.pi * epoch / max_epochs))
+    else:  # stepwise lr schedule
+        for milestone in schedule:
+            lr *= 0.1 if epoch >= milestone else 1.
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
 
 
 def train(train_loader, model, criterion, optimizer, max_epochs, lr, cos, schedule, out_path, model_filename, writer, device, gpu_id=None):
