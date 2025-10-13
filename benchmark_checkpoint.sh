b0a10c75f3c752b93f4e0f0971039663c7ad3d43
Anders Sildnes <andsild@posteo.net>
2025-10-13 17:04:11 +0200

    I think it can be clearly proven that the mental constitution of woman, like that of man, has never failed to rise where restrictions upon equal freedom have been torn down. 		-- Voltairine de Cleyre

--- benchmark_checkpoint.sh
+++ benchmark_checkpoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 base_cmd="ipython train_model/train_ssl.py -- --no-condition --src-dir ~/tiles/ --epochs 10 --moco-k 65536 --out-dir out_seq_ckpt"
-for bs in 32 64 128 256 512
+for bs in 32 64 128 256
 do
-  echo $bs
+  echo "$base_cmd --batch-size $bs"
 done
